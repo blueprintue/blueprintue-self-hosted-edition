9ce642f42a6932f7fde110ee4355d6963c87b400
Rancoud <rancoud@users.noreply.github.com>
2024-01-25 18:45:55 +0100

    feat: add build image workflow (#11)

---
+++ docker-examples/README.md
@@ -0,0 +1,78 @@
+# Examples of docker-compose
+
+## Pull Docker Image from GitHub Registry
+```shell
+docker login ghcr.io
+docker pull ghcr.io/blueprintue/blueprintue-self-hosted-edition:edge
+docker-compose pull
+```
+
+## Docker Compose variations
+For each files you will need to find and replace values `_____REPLACE_ME_____` with what you need.
+
+### Basic
+You will have in that file [docker-compose-localhost.yml]:
+- traefik
+  - open on port 80
+  - use `traefik-http.yml`
+- mariadb
+  - on init use `dump-with-anonymous-user.sql`
+  - use `database.env`
+- maildev
+  - all emails sent are visible on `localhost:1080`
+
+### HTTPS
+You will have in that file [docker-compose-https.yml]:
+- traefik
+  - open on port 80 and 443
+  - use `traefik-https.yml` with let's encrypt challenge for OVH
+  - use `traefik-https.env` with OVH
+- mariadb
+  - on init use `dump-with-anonymous-user.sql`
+  - use `database.env`
+- maildev
+  - all emails sent are visible on `localhost:1080`
+
+### SMTP
+You will have in that file [docker-compose-smtp.yml]:
+- traefik
+  - open on port 80
+  - use `traefik-http.yml`
+- mariadb
+  - on init use `dump-with-anonymous-user.sql`
+  - use `database.env`
+- msmtpd
+  - emails will be sent using smtp relay
+  - use `msmtpd.env`
+
+## Env variables
+### Rootfs
+#### User rights
+* `PUID` user id
+* `PGID` group id
+
+#### Timezone
+* `TZ` timezone (by default: UTC)
+
+#### PHP-FPM
+* `MEMORY_LIMIT` memory limit (by default: 256M)
+* `POST_MAX_SIZE` post max size (by default: 16M)
+* `UPLOAD_MAX_SIZE` upload max size (by default: 16M)
+
+#### OPCache
+* `OPCACHE_ENABLE` opcache enable (by default: 1)
+* `OPCACHE_MEM_SIZE` opcache memory consumption (by default: 128)
+
+#### Nginx
+* `REAL_IP_FROM` real ip from (by default: 0.0.0.0/32)
+* `REAL_IP_HEADER` real ip header (by default: X-Forwarded-For)
+* `LOG_IP_VAR` log ip var (by default: remote_addr)
+
+## Docker Buildx Commands
+* `docker buildx bake` create image-local
+* `docker buildx bake validate` launch 2 subtasks vendor-update && vendor-validate
+* `docker buildx bake vendor-validate` check if there is a drift with composer.lock
+* `docker buildx bake lint` check if code is matching with lint rules
+* `docker buildx bake test` end 2 end testing
+* `docker buildx bake image` create a docker image for registry
+* `docker buildx bake image-local` create a local docker image
---
+++ docker-examples/dump-with-anonymous-user.sql
@@ -0,0 +1,140 @@
+create table if not exists blueprints
+(
+    id int unsigned auto_increment
+    primary key,
+    id_author int unsigned null,
+    slug varchar(100) not null,
+    file_id varchar(100) not null,
+    title varchar(255) not null,
+    type enum('animation', 'behavior_tree', 'blueprint', 'material', 'metasound', 'niagara', 'pcg') default 'blueprint' not null,
+    ue_version char(5) default '4.0' not null,
+    current_version int unsigned not null,
+    thumbnail varchar(255) null,
+    description longtext null,
+    exposure enum('public', 'unlisted', 'private') default 'public' not null,
+    expiration datetime null,
+    tags varchar(255) null,
+    video varchar(255) null,
+    video_provider varchar(255) null,
+    comments_hidden tinyint(1) default '0',
+    comments_closed tinyint(1) default '0',
+    comments_count int unsigned default '0',
+    created_at datetime not null,
+    updated_at datetime null,
+    published_at datetime null,
+    deleted_at datetime null,
+    constraint file_id_UNIQUE
+    unique (file_id),
+    constraint slug_UNIQUE
+    unique (slug)
+    )
+    charset=utf8mb4;
+
+create table if not exists blueprints_version
+(
+    id int unsigned auto_increment
+    primary key,
+    id_blueprint int unsigned not null,
+    version int unsigned not null,
+    reason text not null,
+    created_at datetime not null,
+    updated_at datetime null,
+    published_at datetime null
+)
+    charset=utf8mb4;
+
+create table if not exists comments
+(
+    id int unsigned auto_increment
+    primary key,
+    id_author int unsigned null,
+    id_blueprint int unsigned not null,
+    name_fallback varchar(255) null,
+    content text not null,
+    created_at datetime not null
+    )
+    charset=utf8mb4;
+
+create table if not exists sessions
+(
+    id varchar(128) not null
+    primary key,
+    id_user int unsigned null,
+    last_access datetime not null,
+    content text not null
+    )
+    charset=utf8mb4;
+
+create table if not exists tags
+(
+    id int unsigned auto_increment
+    primary key,
+    name varchar(100) not null,
+    slug varchar(100) not null,
+    constraint slug_UNIQUE
+    unique (slug)
+    )
+    charset=utf8mb4;
+
+create table if not exists users
+(
+    id int unsigned auto_increment
+    primary key,
+    username varchar(100) not null,
+    password text null,
+    slug varchar(100) not null,
+    email varchar(100) null,
+    password_reset varchar(255) null,
+    password_reset_at datetime null,
+    grade enum('member', 'admin') default 'member' not null,
+    avatar varchar(255) null,
+    remember_token char(255) null,
+    created_at datetime not null,
+    confirmed_token char(255) null,
+    confirmed_sent_at datetime null,
+    confirmed_at datetime null,
+    last_login_at datetime null,
+    constraint email_UNIQUE
+    unique (email),
+    constraint username_UNIQUE
+    unique (username),
+    constraint slug_UNIQUE
+    unique (slug),
+    constraint remember_token_UNIQUE
+    unique (remember_token),
+    constraint confirmed_token_UNIQUE
+    unique (confirmed_token)
+    )
+    charset=utf8mb4;
+
+create table if not exists users_api
+(
+    id_user int unsigned not null
+    primary key,
+    api_key varchar(100) not null,
+    constraint api_key_UNIQUE
+    unique (api_key)
+    )
+    charset=utf8mb4;
+
+create table if not exists users_infos
+(
+    id_user int unsigned not null
+    primary key,
+    count_public_blueprint int unsigned default 0 not null,
+    count_public_comment int unsigned default 0 not null,
+    count_private_blueprint int unsigned default 0 not null,
+    count_private_comment int unsigned default 0 not null,
+    bio text null,
+    link_website varchar(255) null,
+    link_facebook varchar(255) null,
+    link_twitter varchar(255) null,
+    link_github varchar(255) null,
+    link_twitch varchar(255) null,
+    link_unreal varchar(255) null,
+    link_youtube varchar(255) null
+    )
+    charset=utf8mb4;
+
+INSERT INTO `users` (`id`, `username`, `password`, `slug`, `email`, `password_reset`, `password_reset_at`, `grade`, `avatar`, `remember_token`, `created_at`, `confirmed_token`, `confirmed_sent_at`, `confirmed_at`) VALUES ('1', 'anonymous', NULL, 'anonymous', NULL, NULL, NOW(), 'member', NULL, NULL, NOW(), NULL, NOW(), NOW());
+INSERT INTO `users_infos` (`id_user`, `count_public_blueprint`, `count_public_comment`, `count_private_blueprint`, `count_private_comment`, `bio`, `link_website`, `link_facebook`, `link_twitter`, `link_github`, `link_twitch`, `link_unreal`, `link_youtube`) VALUES ('1', '0', '0', '0', '0', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
