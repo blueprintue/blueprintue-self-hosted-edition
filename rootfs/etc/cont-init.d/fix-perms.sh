5af32db398f3357a5ebb94742772f9de0f3e5853
Rancoud <rancoud@users.noreply.github.com>
2024-10-11 12:16:36 +0200

    feat: add storage and medias folders (#89)

--- rootfs/etc/cont-init.d/fix-perms.sh
+++ rootfs/etc/cont-init.d/fix-perms.sh
@@ -4,7 +4,6 @@ echo "Fixing perms..."
 mkdir -p /opt/blueprintue-self-hosted-edition/storage \
   /opt/blueprintue-self-hosted-edition/www/medias \
   /opt/blueprintue-self-hosted-edition/www/medias/avatars \
-  /opt/blueprintue-self-hosted-edition/www/medias/posts \
   /opt/blueprintue-self-hosted-edition/www/medias/blueprints \
   /var/run/nginx \
   /var/run/php-fpm
@@ -13,7 +12,6 @@ chown blueprintue-self-hosted-edition. \
   /opt/blueprintue-self-hosted-edition/storage \
   /opt/blueprintue-self-hosted-edition/www/medias \
   /opt/blueprintue-self-hosted-edition/www/medias/avatars \
-  /opt/blueprintue-self-hosted-edition/www/medias/posts \
   /opt/blueprintue-self-hosted-edition/www/medias/blueprints
 chown -R blueprintue-self-hosted-edition. \
   /tpls \
