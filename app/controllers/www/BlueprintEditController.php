325629ddc22504c280d273a6266908056299b46a
Sébastien Rancoud <rancoud@users.noreply.github.com>
2026-02-15 16:44:39 +0100

    fix exposure computation for delete blueprint

--- app/controllers/www/BlueprintEditController.php
+++ app/controllers/www/BlueprintEditController.php
@@ -601,10 +601,10 @@ class BlueprintEditController implements MiddlewareInterface
                 BlueprintService::softDeleteBlueprint($this->blueprintID);
             }
 
-            if ($this->exposure === 'private') {
-                UserService::updatePrivateBlueprintCount($this->userID, -1);
-            } else {
+            if ($this->exposure === 'public') {
                 UserService::updatePublicAndPrivateBlueprintCount($this->userID, -1);
+            } else {
+                UserService::updatePrivateBlueprintCount($this->userID, -1);
             }
 
             /* @noinspection NullPointerExceptionInspection */
